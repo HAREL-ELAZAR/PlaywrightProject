143dc7173ab6562e8b0ab2155b7c14e2ce3fab2c
HAREL-ELAZAR <harelelazar@gmail.com>
2026-02-08 01:52:41 +0200

    add new class for regression tests, add a new yml file , add a new testsng xml file

---
+++ src/test/java/tests/uiTests/CreateUserScreen.java
@@ -0,0 +1,30 @@
+package tests.uiTests;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+
+public class CreateUserScreen extends BaseTest {
+
+
+
+
+    @Test(description = "Test the entity screen")
+    public void test01_CheckTheEntityScreen(){
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
